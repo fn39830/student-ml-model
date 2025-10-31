9b6e50b9f883d62a0df2ed6674c7acf6c6662a14
fn39830 <146485213+fn39830@users.noreply.github.com>
2025-10-31 10:38:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#ouputs "Hello fitness AI!"
 print("Hello fitness AI!")
 
+#import pandas 
 import pandas as pd
-df=pd.read_csv("fitness_data_balanced.csv")
+df=pd.read_csv("fitness_data_balanced.csv") #read dataset
 print(df.head())
 
